7d98653cd4fdd6a46f943b9eb98e198f5090eb85
Brock Rogers <brock.rogers@sharperlending.com>
2021-07-14 11:09:28 -0700

    Added a couple of simple unit tests

---
+++ TriviaServiceUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TriviaServiceUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TriviaServiceUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("73bae218-8366-4b40-9109-9da319c0c38a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TriviaServiceUnitTest/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading;
+using TriviaService;
+using System.Linq;
+using TriviaService.Models;
+using System.Threading.Tasks;
+using System.Web.Http.Results;
+
+namespace TriviaServiceUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public async Task TestNewPlayer()
+        {            
+            var triviaController = new TriviaService.Controllers.TriviaController();            
+            var playerModel = await triviaController.NewPlayer("TestPlayer") as OkNegotiatedContentResult<GamePlayerModel>;                
+            Assert.IsNotNull(playerModel);            
+        }       
+
+        [TestMethod]
+        public void TestAnswer()
+        {
+            var triviaController = new TriviaService.Controllers.TriviaController();
+            var result = triviaController.ValidateAnswer(new TriviaService.Models.AnswerModel
+            {
+                PlayerId = new Guid("79788BC0-AA71-46D4-A01A-28355416E484"),
+                Answer = "C",
+                QuestionId = new Guid("D4FE8314-DD1B-460A-9775-80C59BEF7382")
+            }).Result;
+
+            Assert.IsTrue(result);
+        }
+    }
+}
